07aa6d49f30bd8e46c67dc7b4f47698d17ef0414
prayash51 <114162045+prayash51@users.noreply.github.com>
2023-02-09 00:02:37 +0545

    Initial commit for Strapi CMS

---
+++ config/database.js
@@ -0,0 +1,18 @@
+/*
+ *   Copyright (c) 2023 Prayash Shrestha
+ *   All rights reserved.
+ *   Email
+ */
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'kathmandudaytrips'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'password'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+});
